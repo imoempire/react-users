818ea1a01d3dd746a1c1203c1c03cdea673e4ac0
imoempire <mrimoempire@gmail.com>
2021-07-06 14:52:36 +0000

    clean code

--- src/App.js
+++ src/App.js
@@ -4,41 +4,44 @@ import User from './Components/User';
 import './App.css';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       users: [
-        {name: 'James',
-         email: 'James@gmail.com',
-         gen: 2
+        {
+          name: 'James',
+          email: 'James@gmail.com',
+          gen: 2
         },
-         {name: 'John',
-         email: 'John@gmail.com',
-         gen: 12
+        {
+          name: 'John',
+          email: 'John@gmail.com',
+          gen: 12
         },
-         {name: 'Grace',
-         email: 'Grace@gmail.com',
-         gen: 34
-        }, 
+        {
+          name: 'Grace',
+          email: 'Grace@gmail.com',
+          gen: 34
+        },
       ]
     }
   }
-  handleAddUser = (newUser) => {
-    this.setState({ 
-        users: [...this.state.users, newUser] 
+  handleAddUser = (newUser) => { 
+    this.setState({
+      users: [...this.state.users, newUser]
     })
-};
+  };
   render() {
     const users = this.state.users.map((user, index) => {
       return (
-          <User user={user} index={index}/>
+        <User user={user} index={index} />
       )
-  })
+    })
     return (
       <>
-      <h1>ADD A User</h1>
-       <UseForm addUser={this.handleAddUser} />
-       {users}
+        <h1>ADD A  User</h1>
+        <UseForm addUser={this.handleAddUser} />
+        {users}
       </>
     );
   }
