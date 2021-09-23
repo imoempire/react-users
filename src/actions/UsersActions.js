3f76afca51280301c0c17b67ed003e13f2e08e24
imoempire <mrimoempire@gmail.com>
2021-09-23 12:06:31 +0000

    read data from firebase store

--- src/actions/UsersActions.js
+++ src/actions/UsersActions.js
@@ -3,13 +3,14 @@ import { getFirestore } from "redux-firestore";
 export const ADD_USER = "ADD_USER"
 export const DELETE_USER = "DELETE_USER"
 export const EDIT_USER = "EDIT_USER"
+export const SET_ALL_USERS= "SET_ALL_USERS"
 
 
  export const addUser= (user)=>{
     user.id = Math.random().toString();
     return(dispatch, state, {getFirestore})=>{
         getFirestore()
-        .collection('user')
+        .collection('users')
         .add(user).then((docs)=>{
             console.log(docs)
         });
@@ -31,6 +32,22 @@ export const editUser= (updateUser)=>{
     return {
         type: EDIT_USER,
         payload: updateUser,
+    }
+}
+
+export const getAllUsers =()=>{
+    return(dispatch, state, {getFirestore})=>{
+        getFirestore()
+        .collection('users')
+        .onSnapshot((snapshot)=>{ 
+            let users=[];
+                snapshot.forEach((doc)=>{
+                users.push(doc.data())
+                });
+                dispatch({
+                    type: SET_ALL_USERS,
+                    payload: users })
+            })
     }
 }
 
