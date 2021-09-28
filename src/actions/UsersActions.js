0633369e223f9e15bbe7839ebfeb9ea928b5dc24
imoempire <mrimoempire@gmail.com>
2021-09-28 11:19:20 +0000

    add edit and delete functions

--- src/actions/UsersActions.js
+++ src/actions/UsersActions.js
@@ -11,7 +11,7 @@ export const SET_ALL_USERS= "SET_ALL_USERS"
     return(dispatch, state, {getFirestore})=>{
         getFirestore()
         .collection('users')
-        .add(user).then((docs)=>{
+        .add({...user, timestamp: getFirestore().FieldValue.serverTimestamp()}).then((docs)=>{
             console.log(docs)
         });
     }
@@ -22,27 +22,39 @@ export const SET_ALL_USERS= "SET_ALL_USERS"
 };
 
 export const deleteUser= (userId)=>{
-    return {
-        type: DELETE_USER,
-        payload: userId,
-    }
+    return(dispatch, state, {getFirestore})=>{
+        getFirestore().collection('users')
+        .doc(userId).delete().then(()=>{})
+    } 
+    
+    // {
+    //     type: DELETE_USER,
+    //     payload: userId,
+    // }
 };
 
 export const editUser= (updateUser)=>{
-    return {
-        type: EDIT_USER,
-        payload: updateUser,
+    return(dispatch, state, {getFirestore})=>{
+        getFirestore().collection('users')
+        .doc(updateUser.id).set(updateUser)
+        .then(()=>{})
     }
+
+    //  {
+    //     type: EDIT_USER,
+    //     payload: updateUser,
+    // }
 }
 
 export const getAllUsers =()=>{
     return(dispatch, state, {getFirestore})=>{
         getFirestore()
         .collection('users')
+        .orderBy('timestamp','desc')
         .onSnapshot((snapshot)=>{ 
             let users=[];
                 snapshot.forEach((doc)=>{
-                users.push(doc.data())
+                users.push({...doc.data(), id:doc.id})
                 });
                 dispatch({
                     type: SET_ALL_USERS,
